d4c4a4050987635254862f5083f57daffeba7710
Kishore Sohanda <kishoresohanda100@gmail.com>
2024-03-06 16:36:55 +0530

    small http exception error resolved and handler function for httpCreateServer is now in seprate file named routes.js

--- routes.js
+++ routes.js
@@ -27,13 +27,14 @@ const requestHandler =(req,res)=>{
                 return res.end();
             });
         });
-    }
+    }else{
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello from my node.js server</h1></body>');
     res.write('</html>');
     res.end();
+    }
 };
 
 module.exports= requestHandler;
